683bb6558bd3da3c695646cd2d76920e79276941
Ben Bristow <ben@bristow.me>
2022-06-01 22:27:57 +0100

    refactor: Remove Azure AI

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import './scss/main.scss';
 
-import { ApplicationInsights } from '@microsoft/applicationinsights-web';
-
 import createContactApp from './contact';
-
-const appInsights = new ApplicationInsights({
-    config: {
-    instrumentationKey: 'bb397acc-54ed-4f2e-a08e-9e8d1ad6e3d2'
-    }
-});
-
-appInsights.loadAppInsights();
-appInsights.trackPageView();
 
 window.addEventListener('load', () => {
     const contactElement = document.getElementById('bb-js-contact');
