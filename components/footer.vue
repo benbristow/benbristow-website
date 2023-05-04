a8028f6e3dcfdfe4787a09b758d13e22a9366566
Benjamin Bristow <ben@bristow.me>
2023-05-04 23:39:52 +0100

    feat: footer social icons

--- components/footer.vue
+++ components/footer.vue
@@ -1,11 +1,56 @@
-
 <template>
     <footer class="bg-light text-center py-5 mt-5">
         <div class="container">
-            <p>&copy; Ben Bristow {{ currentYear }}</p>
+            <div class="social mb-2">
+                <a class="mx-2" href="https://www.linkedin.com/in/benbb" title="My LinkedIn" target="_blank"
+                   rel="noopener noreferrer">
+                    <i class="fa fa-linkedin"></i>
+                </a>
+
+                <a class="mx-2" href="https://www.instagram.com/benbrist" title="My Instagram" target="_blank"
+                   rel="noopener noreferrer">
+                    <i class="fa fa-instagram"></i>
+                </a>
+
+                <a class="mx-2" href="https://www.twitter.com/benbristowuk" title="My Twitter" target="_blank"
+                   rel="noopener noreferrer">
+                    <i class="fa fa-twitter"></i>
+                </a>
+
+                <a class="mx-2" href="https://www.flickr.com/photos/benbrist" title="My Flickr" target="_blank"
+                   rel="noopener noreferrer">
+                    <i class="fa fa-camera"></i>
+                </a>
+
+                <a class="mx-2" href="https://www.github.com/benbristow" title="My GitHub" target="_blank"
+                   rel="noopener noreferrer">
+                    <i class="fa fa-github"></i>
+                </a>
+
+                <a class="mx-2" href="https://www.youtube.com/channel/UCO8AfKh3KGeKqRpub2m1vAQ"
+                   title="My YouTube Channel" target="_blank" rel="noopener noreferrer">
+                    <i class="fa fa-youtube-play"></i>
+                </a>
+            </div>
+
+            &copy; Ben Bristow {{ currentYear }}
         </div>
     </footer>
 </template>
+
+<style scoped lang="scss">
+.social {
+  font-size: 1.8rem;
+
+  a {
+    text-decoration: none;
+
+    :hover {
+      color: lighten(#000, 50%);
+    }
+  }
+}
+</style>
 
 <script setup lang="ts">
 const currentYear = new Date().getFullYear();
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ export default defineNuxtConfig({
         head: {
             link: [
                 { rel: 'preload', as: 'style', type: 'text/css', href: '//fonts.googleapis.com/css2?family=Sarala:wght@400;700&display=swap' },
-                { rel: 'stylesheet', type: 'text/css', href: '//fonts.googleapis.com/css2?family=Sarala:wght@400;700&display=swap' }
+                { rel: 'stylesheet', type: 'text/css', href: '//fonts.googleapis.com/css2?family=Sarala:wght@400;700&display=swap' },
+                { rel: 'stylesheet', type: 'text/css', href: 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', crossorigin: 'anonymous', integrity: 'sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN' }
             ],
             titleTemplate: '%s - ' + 'Ben Bristow',
         },
