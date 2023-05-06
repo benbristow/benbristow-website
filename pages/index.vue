87e18c713da82a538140e36d82d8c77c68cf4342
Benjamin Bristow <ben@bristow.me>
2023-05-06 14:19:13 +0100

    refactor: Use &amp;

--- pages/index.vue
+++ pages/index.vue
@@ -9,16 +9,16 @@
         </div>
         <div class="col-lg-9">
             <h2>Hello, world!</h2>
-            <p>I'm a full-stack software engineer and 'Web & Mobile Development (Bsc)' graduate from the University of
+            <p>I'm a full-stack software engineer and 'Web &amp; Mobile Development (Bsc)' graduate from the University of
                 the West of Scotland (Paisley Campus). I was originally born in Grimsby, England and I'm a big Grimsby
                 Town Football Club fan!</p>
             <p>I currently work for Ofgem, the UK government's 'Office of Gas and Electricity Markets' regulator based
                 in their office in Glasgow City Centre, working in a hybrid capacity as a principal software engineer
-                working with modern web technologies including C#/ASP.NET (.NET 7) following agile & DevOps
+                working with modern web technologies including C#/ASP.NET (.NET 7) following agile &amp; DevOps
                 methodology.</p>
-            <p>In my free-time I like to work on pet-projects and travel (mainly by rail), creating video content &
-                taking photographs. I also love listening to music & attending live events.</p>
-            <p>Please feel free to browse my website & other projects listed. If you wish to get in touch, please
+            <p>In my free-time I like to work on pet-projects and travel (mainly by rail), creating video content &amp;
+                taking photographs. I also love listening to music &amp; attending live events.</p>
+            <p>Please feel free to browse my website &amp; other projects listed. If you wish to get in touch, please
                 contact me via the contact details on my CV or contact page, or via social media using the icons in the
                 footer.</p>
         </div>
