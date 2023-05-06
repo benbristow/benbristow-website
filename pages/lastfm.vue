892d43e5d50a035a436cb4d556b09ea343d9581f
Benjamin Bristow <ben@bristow.me>
2023-05-06 15:50:54 +0100

    refactor: last.fm casing

--- pages/lastfm.vue
+++ pages/lastfm.vue
@@ -1,6 +1,6 @@
 <template>
     <Head>
-        <Title>Last.FM</Title>
+        <Title>Last.fm</Title>
     </Head>
 
     <h2 class="mb-5"><i class="fa fa-lastfm" aria-hidden="true"></i> My current <a href="https://last.fm/user/benbristow" rel="noopener noreferrer" target="_blank">Last.fm</a> top tracks</h2>
