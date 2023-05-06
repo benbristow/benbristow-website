65c7e91b7b883dbcbff5c00973bca5e72127e722
Benjamin Bristow <ben@bristow.me>
2023-05-06 15:25:05 +0100

    feat: Last.fm in footer.vue

--- components/footer.vue
+++ components/footer.vue
@@ -4,32 +4,37 @@
             <div class="social mb-2">
                 <a class="mx-2" href="https://www.linkedin.com/in/benbb" title="My LinkedIn" target="_blank"
                    rel="noopener noreferrer">
-                    <i class="fa fa-linkedin"></i>
+                    <i class="fa fa-linkedin" aria-hidden="true"></i>
                 </a>
 
                 <a class="mx-2" href="https://www.instagram.com/benbrist" title="My Instagram" target="_blank"
                    rel="noopener noreferrer">
-                    <i class="fa fa-instagram"></i>
+                    <i class="fa fa-instagram" aria-hidden="true"></i>
                 </a>
 
                 <a class="mx-2" href="https://www.twitter.com/benbristowuk" title="My Twitter" target="_blank"
                    rel="noopener noreferrer">
-                    <i class="fa fa-twitter"></i>
+                    <i class="fa fa-twitter" aria-hidden="true"></i>
                 </a>
 
                 <a class="mx-2" href="https://www.flickr.com/photos/benbrist" title="My Flickr" target="_blank"
                    rel="noopener noreferrer">
-                    <i class="fa fa-camera"></i>
+                    <i class="fa fa-camera" aria-hidden="true"></i>
                 </a>
 
                 <a class="mx-2" href="https://www.github.com/benbristow" title="My GitHub" target="_blank"
                    rel="noopener noreferrer">
-                    <i class="fa fa-github"></i>
+                    <i class="fa fa-github" aria-hidden="true"></i>
                 </a>
 
                 <a class="mx-2" href="https://www.youtube.com/channel/UCO8AfKh3KGeKqRpub2m1vAQ"
                    title="My YouTube Channel" target="_blank" rel="noopener noreferrer">
-                    <i class="fa fa-youtube-play"></i>
+                    <i class="fa fa-youtube-play" aria-hidden="true"></i>
+                </a>
+
+                <a class="mx-2" href="https://www.last.fm/user/benbristow" title="My Last.fm" target="_blank"
+                   rel="noopener noreferrer">
+                    <i class="fa fa-lastfm" aria-hidden="true"></i>
                 </a>
             </div>
 
