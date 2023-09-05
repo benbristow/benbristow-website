b69e46e76ab7b3b2482f376dc2f5e206388da1c5
Ben Bristow <ben@bristow.me>
2023-09-05 18:24:56 +0100

    refactor: remove captcha badge

--- components/contact-form.vue
+++ components/contact-form.vue
@@ -44,6 +44,10 @@
                         rows="10"
                 />
             </div>
+
+            <div class="mb-3">
+                This site is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy" target="_blank" rel="noopener noreferrer">Privacy Policy</a> and <a href="https://policies.google.com/terms" target="_blank" rel="noopener noreferrer">Terms of Service</a> apply.
+            </div>
         </FormKit>
 
         <div v-else>
