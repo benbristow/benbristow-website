1f40aa3926c0e9bbf25ecd7d3800b135f02e93a0
Ben Bristow <ben@bristow.me>
2024-07-12 22:59:25 +0100

    feat: add analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,10 @@ export default defineNuxtConfig({
             link: [
                 { rel: 'preload', as: 'style', type: 'text/css', href: '//fonts.googleapis.com/css2?family=Sarala:wght@400;700&display=swap' },
                 { rel: 'stylesheet', type: 'text/css', href: '//fonts.googleapis.com/css2?family=Sarala:wght@400;700&display=swap' },
-                { rel: 'stylesheet', type: 'text/css', href: 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', crossorigin: 'anonymous', integrity: 'sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN' }
+                { rel: 'stylesheet', type: 'text/css', href: 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', crossorigin: 'anonymous', integrity: 'sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN' },
+            ],
+            script: [
+                { src: 'https://analytics.srv.bristow.me/js/script.js', defer: true, 'data-domain': 'ben.bristow.me' },
             ],
             titleTemplate: '%s - ' + 'Ben Bristow',
         },
